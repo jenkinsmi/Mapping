64749506e8dc298e7eb665d19fb0982a22a4e6cf
jenkinsmi <jenkinsmi@hotmail.com>
2018-02-01 12:51:50 +0000

    Added TextViews and EditTexts with longitude and latitude

--- app/src/main/java/com/example/a2jenkm59/mapping/MainActivity.java
+++ app/src/main/java/com/example/a2jenkm59/mapping/MainActivity.java
@@ -7,13 +7,17 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements OnClickListener
 {
 
     MapView mv;
@@ -35,5 +39,17 @@ public class MainActivity extends AppCompatActivity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
         mv.getController().setCenter(new GeoPoint(50.913639,-1.411781));
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
     }
+    public void onClick(View view)
+    {
+
+        EditText lattext = (EditText)findViewById(R.id.et1);
+        EditText longtext = (EditText)findViewById(R.id.et2);
+        double latitude = Double.parseDouble(lattext.getText().toString());
+        double longitude = Double.parseDouble(longtext.getText().toString());
+        mv.getController().setCenter(new GeoPoint(latitude,longitude));
+    }
+
 }
